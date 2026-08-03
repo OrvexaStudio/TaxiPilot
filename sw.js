f01105eff00a387951793514fb5eb51e4ad37d8f
OrvexaStudio <phw966j56w@privaterelay.appleid.com>
2026-08-03 02:25:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,8 +22,7 @@ self.addEventListener(
 "install",
 event=>{
 
-self.skipWaiting();
-  
+
 event.waitUntil(
 
 caches.open(cacheName)
@@ -33,45 +32,6 @@ caches.open(cacheName)
 return cache.addAll(files);
 
 })
-
-);
-
-
-});
-
-self.addEventListener(
-"activate",
-event=>{
-
-
-event.waitUntil(
-
-caches.keys()
-
-.then(keys=>{
-
-
-return Promise.all(
-
-keys.map(key=>{
-
-
-if(
-key !== cacheName
-){
-
-return caches.delete(key);
-
-}
-
-
-})
-
-);
-
-
-})
-
 
 );
 
