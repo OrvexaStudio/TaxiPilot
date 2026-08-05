76771b4e523832c2d8be2b6aa8ba56a22bdde870
OrvexaStudio <phw966j56w@privaterelay.appleid.com>
2026-08-05 02:51:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -61,3 +61,39 @@ return response || fetch(event.request);
 
 
 });
+
+self.addEventListener(
+"activate",
+event=>{
+
+event.waitUntil(
+
+caches.keys()
+
+.then(cacheNames=>{
+
+return Promise.all(
+
+cacheNames.map(cache=>{
+
+if(cache !== cacheName){
+
+return caches.delete(cache);
+
+}
+
+})
+
+);
+
+})
+
+.then(()=>{
+
+return clients.claim();
+
+})
+
+);
+
+});
